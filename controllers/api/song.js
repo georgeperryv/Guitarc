6fc8a0a22f1f0bded3c3f304a790998bbb9cb2c4
George Perry <george.e.perry5@gmail.com>
2022-10-11 21:53:11 -0700

    correctly posting new songs which include a reference to their associated category

--- controllers/api/song.js
+++ controllers/api/song.js
@@ -1,13 +1,19 @@
 const Song = require('../../models/song')
+const Category = require('../../models/category')
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 
 async function addSong (req, res) {
-  console.log('This is req', req)
-
   try {
     // const collection = await Collection.create(req.body)
-    const song = new Song(req.body)
+    // console.log(
+    //   'finding all in category',
+    //   await Category.findOne({ category: req.body.activeCat })
+    // )
+    const song = new Song({
+      song: req.body.song,
+      category: await Category.findOne({ category: req.body.activeCat })
+    })
     await song.save()
 
     return res.json(song)
